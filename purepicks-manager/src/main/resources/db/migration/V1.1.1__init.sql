a28af53658096d10031e7f8eafe6fc79fd045051
devSurfer <=>
2025-01-10 00:00:26 +0800

    新功能(manager): 添加flyway数据库版本控制

---
+++ purepicks-manager/src/main/resources/db/migration/V1.1.1__init.sql
@@ -0,0 +1,3 @@
+# create database purepicks_manager charset utf8mb4;
+# create user 'purepicks_manager_user'@'localhost' identified by 'devSurfer';
+# grant all privileges on purepicks_manager.* to 'purepicks_manager_user'@'localhost';
