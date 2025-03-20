22cca49193c62074846fb0bfd1130a42d46939d1
devSurfer <=>
2025-03-20 23:01:30 +0800

    工程: gateway网关服务搭建

---
+++ purepicks-server-gateway/src/main/java/com/devsurfer/purepicks/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.devsurfer.purepicks.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dev Surfer
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
