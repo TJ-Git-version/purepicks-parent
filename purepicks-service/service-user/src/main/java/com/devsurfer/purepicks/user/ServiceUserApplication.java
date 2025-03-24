1633f7ff3d5f6f0c2c84c87d683491536613657d
devSurfer <=>
2025-03-24 23:32:57 +0800

    新功能(user): 启用用户登录拦截器注解

--- purepicks-service/service-user/src/main/java/com/devsurfer/purepicks/user/ServiceUserApplication.java
+++ purepicks-service/service-user/src/main/java/com/devsurfer/purepicks/user/ServiceUserApplication.java
@@ -1,8 +1,10 @@
 package com.devsurfer.purepicks.user;
 
+import com.devsurfer.purepicks.service.annotation.EnableUserWebMvcConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 
@@ -15,9 +17,14 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.devsurfer.purepicks.user.mapper")
 @ComponentScan(basePackages = "com.devsurfer.purepicks")
+@EnableUserWebMvcConfiguration
 public class ServiceUserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceUserApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ServiceUserApplication.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
         System.out.println("http://localhost:8200/doc.html");
     }
 }
